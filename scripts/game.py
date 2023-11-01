137c6d1a9a85d0ebe3087c19b34c7c6a8e4666bf
unknown <kemaspasha9@gmail.com>
2023-11-01 12:13:50 +0930

    slight adjustment

--- scripts/game.py
+++ scripts/game.py
@@ -7,12 +7,16 @@ class Game:
         self.surface = pygame.Surface((GAME_WIDTH, GAME_HEIGHT))
         self.display_surface = pygame.display.get_surface()
         self.rect = self.surface.get_rect(topleft = (PADDING, PADDING))
+        self.sprites = pygame.sprite.Group()
 
         # lines
         self.line_surface = self.surface.copy()
         self.line_surface.fill((0, 255, 0))
         self.line_surface.set_colorkey((0, 255, 0))
         self.line_surface.set_alpha(120)
+
+        # test
+        self.block = Block(self.sprites, pygame.Vector2(3, 5), 'red')
 
     def draw_grid(self):
         for col in range(1, COLLUMNS):
@@ -29,7 +33,28 @@ class Game:
 
         # drawing
         self.surface.fill(LIGHT_SLATE_GRAY)
+        self.sprites.draw(self.surface)
 
         self.draw_grid()
         self.display_surface.blit(self.surface, (PADDING,PADDING))
         pygame.draw.rect(self.display_surface, WHITE, self.rect, 2, 2)
+
+class Tetromino:
+    def __init__(self, shape, group):
+        
+        # initial setup
+        self.block_position
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, group, pos, color):
+        super().__init__(group)
+
+        # general
+        self.image = pygame.Surface((CELL_SIZE, CELL_SIZE))
+        self.image.fill(color)
+
+        # position
+        self.pos = pos
+        x = self.pos.x * CELL_SIZE
+        y = self.pos.y * CELL_SIZE
+        self.rect = self.image.get_rect(topleft = (x, y))
