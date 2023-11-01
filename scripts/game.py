31031f515227b342f75e574f036d6d53ec3fd9a6
unknown <kemaspasha9@gmail.com>
2023-11-02 00:18:34 +0930

    collisions

--- scripts/game.py
+++ scripts/game.py
@@ -19,15 +19,28 @@ class Game:
         self.line_surface.set_alpha(120)
 
         # tetromino
+        self.field_data = [[0 for x in range(COLLUMNS)] for y in range(ROWS)]
         random_shapes = choice(list(TETROMINOS.keys()))
-        self.tetromino = Tetromino(random_shapes, self.sprites)
+        self.tetromino = Tetromino(
+        random_shapes, 
+        self.sprites, 
+        self.create_new_tetromino, 
+        self.field_data)
 
         # timer
         self.timers = {
-            'vertical move' : Timer(UPDATE_START_SPEED, True, self.move_down)
+            'vertical move' : Timer(UPDATE_START_SPEED, True, self.move_down),
+            'horizontal move' : Timer(MOVE_WAIT_TIME)
 
         }
         self.timers['vertical move'].activate()
+
+    def create_new_tetromino(self):
+        self.tetromino = Tetromino(
+        choice(list(TETROMINOS.keys())), 
+        self.sprites, 
+        self.create_new_tetromino,
+        self.field_data)
 
     def timer_update(self):
         for timer in self.timers.values():
@@ -47,9 +60,22 @@ class Game:
 
         self.surface.blit(self.line_surface, (0, 0))
 
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if not self.timers['horizontal move'].active:
+            if keys[pygame.K_LEFT]:
+                self.tetromino.move_horizontal(-1 )
+                self.timers['horizontal move'].activate()
+            if keys[pygame.K_RIGHT] :
+                self.tetromino.move_horizontal(1)
+                self.timers['horizontal move'].activate()
+            
+
     def run(self):
 
         # update
+        self.input()
         self.timer_update()
         self.sprites.update()
 
@@ -62,18 +88,41 @@ class Game:
         pygame.draw.rect(self.display_surface, WHITE, self.rect, 2, 2)
 
 class Tetromino:
-    def __init__(self, shape, group):
+    def __init__(self, shape, group, create_new_tetromino, field_data):
         
         # initial setup
         self.block_positions = TETROMINOS[shape]['shape']
         self.color = TETROMINOS[shape]['color']
+        self.create_new_tetromino = create_new_tetromino
+        self.field_data = field_data
 
         # create blocks
         self.blocks = [Block(group, pos, self.color) for pos in self.block_positions]
 
+    # collision
+    def next_horizontal_move_colision(self, block, amount):
+        collison_list = [block.horizontal_collide(int(block.pos.x + amount), self.field_data) for block in self.blocks]
+        return True if any(collison_list) else False
+
+    def next_vertical_move_collision(self, block):
+        collision_list = [block.vertical_collide(int(block.pos.y + 1), self.field_data) for block in self.blocks]
+        return True if any(collision_list) else False
+
+    # movement
+    def move_horizontal(self, amount):
+        if not self.next_horizontal_move_colision(self.blocks, amount):
+            for block in self.blocks:
+                block.pos.x += amount
+
     def move_down(self):
-        for block in self.blocks:
-            block.pos.y += 1
+        if not self.next_vertical_move_collision(self.blocks):
+            for block in self.blocks:
+                block.pos.y += 1
+
+        else:
+            for block in self.blocks:
+                self.field_data[int(block.pos.y)][int(block.pos.x)] = 1
+            self.create_new_tetromino()
 
 class Block(pygame.sprite.Sprite):
     def __init__(self, group, pos, color):
@@ -86,6 +135,20 @@ class Block(pygame.sprite.Sprite):
         # position
         self.pos = pygame.Vector2(pos) + BLOCK_OFFSET
         self.rect = self.image.get_rect(topleft = self.pos * CELL_SIZE)
+
+    def horizontal_collide(self, x, field_data):
+        if not 0 <= x < COLLUMNS:
+            return True
+
+        if field_data[int(self.pos.y)][x]:
+            return True
+
+    def vertical_collide(self, y, field_data):
+        if not y < ROWS:
+            return True
+
+        if y >= 0 and field_data[y][int(self.pos.x)]:
+            return True
 
     def update(self):
         self.rect.topleft = self.pos * CELL_SIZE
